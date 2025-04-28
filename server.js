349d95329f3fe3476318b84d69c6b4c9fed85e2e
DN242005 <paz76242@gamil.com>
2025-04-28 16:00:03 -0600

    Subiendo cambios del backend corregido

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Importa las rutas
+const userRoutes = require('./routes/userRoutes');
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Montar las rutas en '/api'
+app.use('/api', userRoutes);
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
